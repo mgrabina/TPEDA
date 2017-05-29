e48a22ff6c75dd19efcef465be45721bd2a705c4
juan grethe <jgrethe@itba.edu.ar>
2017-05-29 14:04:04 -0300

    merge

--- src/controller/Listeners.java
+++ src/controller/Listeners.java
@@ -14,13 +14,21 @@ public class Listeners {
 
 
 	public static void llamadaDeCasillero(int fil, int col){
-		if(game.mover(fil, col, game.getNext())){
-			GUI.ponerFicha(((game.getNext().getColor()==true)?true:false), fil, col);
-			game.getTablero().agregarFicha(game.getNext(), fil, col);
+		if(game.validarMovimiento(fil, col, game.getNext())){
+			agregarFicha(fil, col);
 			game.setNext((game.getNext().esMaquina()==true)?game.getPersona():game.getMaquina());
-			
 		}else{
 			
 		}
 	}
+	
+	private static void agregarFicha(int fil, int col){
+		GUI.ponerFicha(((game.getNext().getColor()==true)?true:false), fil, col);
+		game.getTablero().agregarFicha(game.getNext(), fil, col);
+	}
+	
+	
+	
+	
+	
 }
