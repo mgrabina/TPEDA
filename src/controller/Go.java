50d39ea4da54c536ac09cf2c2503845fd32c4317
Cata Varela <cavarela@itba.edu.ar>
2017-05-31 00:01:31 -0300

    correccion pequeña parte del codigo

--- src/controller/Go.java
+++ src/controller/Go.java
@@ -70,7 +70,7 @@ public class Go {
 		if(a){
 			tablero.agregarFicha(j, fila, columna);
 			for(Ficha fic: fichasacomer){
-				comer(fic.getFila(),fic.getColumna(),fic.getColor(),next);
+				comer(fic.getFila(),fic.getColumna(),fic.getColor());
 			}
 			System.out.println("PuntosCP:" + maquina.getPuntos() + ", puntosPlayer:" + persona.getPuntos());
 			return true;
@@ -95,30 +95,29 @@ public class Go {
 		int arr = (fila == 0) ? 0 : 1;
 		int abj = (12 == fila) ? 0 : 1;
 		boolean izqB = false, derB = false, arrB = false, abjB = false;
-		Jugador oponente = j.esMaquina()? persona : maquina;
 		
 		if(izq != 0){
 			f = tablero.getFicha(fila, columna - 1);
 			if(f != null && f.getColor() != j.getColor())
-				izqB = intentarComer(fila, columna - 1, oponente.getColor(), oponente);
+				izqB = intentarComer(fila, columna - 1, !j.getColor());
 		}
 		
 		if(der != 0){
 			f = tablero.getFicha(fila, columna + 1);
 			if(f != null && f.getColor() != j.getColor())
-				derB = intentarComer(fila, columna + 1, oponente.getColor(), oponente);
+				derB = intentarComer(fila, columna + 1, !j.getColor());
 		}
 		
 		if(arr != 0){
 			f = tablero.getFicha(fila - 1, columna);
 			if(f != null && f.getColor() != j.getColor())
-				arrB = intentarComer(fila - 1, columna, oponente.getColor(), oponente);
+				arrB = intentarComer(fila - 1, columna, !j.getColor());
 		}
 		
 		if(abj != 0){
 			f = tablero.getFicha(fila + 1, columna);
 			if(f != null && f.getColor() != j.getColor())
-				abjB = intentarComer(fila + 1, columna, oponente.getColor(), oponente);
+				abjB = intentarComer(fila + 1, columna, !j.getColor());
 		}
 		
 		tablero.sacarFicha(fila, columna);
@@ -127,7 +126,7 @@ public class Go {
 		
 	}
 
-	private boolean intentarComer(int fila, int columna, boolean color, Jugador j) {
+	private boolean intentarComer(int fila, int columna, boolean color) {
 		if(!tieneLibertad(fila, columna, color, new LinkedList<Ficha>())){
 				fichasacomer.add(new Ficha(color, fila, columna));
 				return true;
@@ -136,7 +135,7 @@ public class Go {
 		return false;
 	}
 
-	private void comer(int fila, int columna, boolean color, Jugador j) {
+	private void comer(int fila, int columna, boolean color) {
 		int izq, der, arr, abj;
 		Ficha f;
 		
@@ -145,39 +144,34 @@ public class Go {
 		arr = (fila == 0) ? 0 : 1;
 		abj = (12 == fila) ? 0 : 1;
 		
-		if(j.esMaquina())
+		if(color)
 			maquina.agregarPuntos();
 		else
 			persona.agregarPuntos();
+		
 		tablero.sacarFicha(fila, columna);
 		GUI.sacarFicha(fila, columna);
 		
 		for (int l = columna - izq; l <= columna + der; l++) {
 			f = tablero.getFicha(fila, l);
 			if (f != null && f.getColor() == color) {
-				comer(fila, l, color, j);
+				comer(fila, l, color);
 			}
 		}
 		
 		if (arr == 1) {
 			f = tablero.getFicha(fila - 1, columna);
 			if (f != null && f.getColor() == color) {
-				if(j.esMaquina())
-				comer(fila - 1, columna, color, j);
+				comer(fila - 1, columna, color);
 			}
 		}
 		
 		if (abj == 1) {
 			f = tablero.getFicha(fila + 1, columna);
 			if (f != null && f.getColor() == color) {
-				comer(fila + 1, columna, color, j);
+				comer(fila + 1, columna, color);
 			}
 		}
-	}
-
-	private boolean noEsKo(int fila, int columna, Jugador j) {
-		// TODO Auto-generated method stub
-		return true;
 	}
 
 	private boolean noEsSuicidio(int fila, int columna, Jugador j) {
